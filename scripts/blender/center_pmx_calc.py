c27d0a6c839ebbc63a8f814881a486904e68e6d7
capric98 <42015599+capric98@users.noreply.github.com>
2024-02-26 20:23:33 -0600

    fix calculation

--- scripts/blender/center_pmx_calc.py
+++ scripts/blender/center_pmx_calc.py
@@ -2,13 +2,16 @@
 # coding: utf-8
 
 # Data from CopanoRickey fbx scale at 100.
-uma_dx = 0
-uma_dy = 0.023805 - 0.034437
-uma_dz = 0.989637 - 0.908125
+# uma_dx = 0
+# uma_dy = 0.023805 - 0.034437
+# uma_dz = 0.989637 - 0.908125
 
 uma_center_z = 0.908125
 uma_thigh = 0.352816
 uma_leg   = 0.402078
+
+uma_center_pos = (0, 11.35156, 0.4304609)
+uma_center_off = (0, 1.018911, -0.1329028)
 
 _FIXED_FACTOR_ = None
 
@@ -24,17 +27,25 @@ if __name__ == "__main__":
     thigh = ((hip_x-knee_x)**2 + (hip_y-knee_y)**2 + (hip_z-knee_z)**2) ** 0.5
     leg   = ((foot_x-knee_x)**2 + (foot_y-knee_y)**2 + (foot_z-knee_z)**2) ** 0.5
 
-    mmd_factor = leg / uma_leg if not _FIXED_FACTOR_ else _FIXED_FACTOR_
+    mmd_factor = leg / uma_leg
     thigh_factor = thigh / uma_thigh
 
-    print(f"scale factor: {mmd_factor:.2f}")
+    print(f"leg factor: {mmd_factor:.2f}")
     print(f"thigh factor: {thigh_factor:.2f}")
 
-    pos_y = uma_center_z * mmd_factor # see below
+    mmd_factor = (leg+thigh)/(uma_leg+uma_thigh) if not _FIXED_FACTOR_ else _FIXED_FACTOR_
 
-    offset_x = uma_dx * mmd_factor
-    offset_y = uma_dz * mmd_factor # Blender coordinates -> PMX Editor coordinates
-    offset_z = uma_dy * mmd_factor # Blender coordinates -> PMX Editor coordinates
+    print(f"\nscale factor: {mmd_factor:.2f}")
 
-    print(f"新的センター Position：0 {pos_y} {hip_z}")
+    mmd_factor /= 12.5
+
+    pos_x = uma_center_pos[0] * mmd_factor
+    pos_y = uma_center_pos[1] * mmd_factor # see below
+    pos_z = uma_center_pos[2] * mmd_factor
+
+    offset_x = uma_center_off[0] * mmd_factor
+    offset_y = uma_center_off[1] * mmd_factor
+    offset_z = uma_center_off[2] * mmd_factor
+
+    print(f"新的センター Position：{pos_x} {pos_y} {pos_z}")
     print(f"新的センター offset：{offset_x} {offset_y} {offset_z}")
